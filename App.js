ddb9851aff9c4f3bc64677cc7493304dd82ccb63
gellangolosino <golosino845@gmail.com>
2025-09-26 09:51:14 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,23 +1,34 @@
+import ChatApp from './ChatApp';
 import CommentApp from './CommentApp';
-
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import CounterApp from './ChatApp';
-import ColorChangerApp from './CommentApp';
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <CommentApp />
- />
-      <ChatApp/>
+      {/* Chat Section */}
+      <View style={styles.section}>
+        <CommentApp/>
+      </View>
+
+      {/* Comment Section */}
+      <View style={styles.section}>
+        <ChatApp/>
+
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1, 
+    flexDirection: 'column', 
+  },
+  section: {
     flex: 1,
+    borderWidth: 1, 
+    borderColor: '#ccc',
   },
 });
 
